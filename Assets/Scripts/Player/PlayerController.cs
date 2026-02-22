a3cda0ec384c97bfaec913356547b3b613161a86
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2026-02-22 22:52:16 +0100

    feat: add player jumping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -3,6 +3,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5.0f;
+    public float jumpforce = 5.0f;
+
+    public LayerMask groundLayer;
+    private bool isGrounded;
+
     Rigidbody rb;
     Vector3 direction;
 
@@ -16,10 +21,19 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         direction = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+        }
     }
 
     private void FixedUpdate()
     {
         rb.linearVelocity = direction * speed;
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position, Vector3.down, 1.1f, groundLayer);
     }
 }
