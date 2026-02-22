b0bd758e16c7342707ce6d0b778254618387ea01
Stefan Josipovic <stefan.josipovic2005@gmail.com>
2026-02-22 23:47:27 +0100

    feat: add enemy spawner

---
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs;
+    public Transform playerTransform;
+    public float spawnRadius;
+    public float spawnInterval;
+    public float timeSinceStart;
+    public float currentTime;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceStart += Time.deltaTime;
+        currentTime += Time.deltaTime;
+        if(currentTime >= spawnInterval)
+        {
+            currentTime -= spawnInterval;
+            float angle = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+            Vector3 spawnPos = new Vector3(playerTransform.position.x + Mathf.Cos(angle) * spawnRadius,
+                2, playerTransform.position.z + Mathf.Sin(angle) * spawnRadius);
+            if (timeSinceStart >= 120f)
+            {
+                SpawnGameObject(enemyPrefabs[2], spawnPos);
+            } else if(timeSinceStart >= 60f)
+            {
+                SpawnGameObject(enemyPrefabs[1], spawnPos);
+            } else if(timeSinceStart >= 5f)
+            {
+                SpawnGameObject(enemyPrefabs[0], spawnPos);
+            }
+        }
+    }
+
+    private void SpawnGameObject(GameObject enemy, Vector3 position)
+    {
+        Instantiate(enemy, position, Quaternion.identity);
+    }
+}
